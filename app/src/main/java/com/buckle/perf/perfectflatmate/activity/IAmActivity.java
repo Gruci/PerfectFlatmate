4b47ec7829295edf17a044550f369f56deaf0c7e
Gruci <skeksksk@gmail.com>
2017-04-09 13:17:30 +0900

    ?

--- app/src/main/java/com/buckle/perf/perfectflatmate/activity/IAmActivity.java
+++ app/src/main/java/com/buckle/perf/perfectflatmate/activity/IAmActivity.java
@@ -1,17 +1,26 @@
 package com.buckle.perf.perfectflatmate.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.buckle.perf.perfectflatmate.BaseActivity;
 import com.buckle.perf.perfectflatmate.R;
 import com.tsengvn.typekit.TypekitContextWrapper;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class IAmActivity extends BaseActivity{
+
+    @BindView(R.id.next_btn)
+    Button mNextBtn;
 
     private Toolbar toolbar;
 
@@ -19,10 +28,20 @@ public class IAmActivity extends BaseActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_i_am);
+        ButterKnife.bind(this);
 
         toolbar = (Toolbar)findViewById(R.id.app_bar);
         ((TextView) findViewById(R.id.toolbar_title)).setText("I am ...");
 
         setSupportActionBar(toolbar);
+
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IAmActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
